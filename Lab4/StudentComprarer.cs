37cc02152a1ecc49231659445f4aa38ef302af1f
anastasiia409 <64258246+anastasiia409@users.noreply.github.com>
2022-05-26 07:57:33 +0100

    Add files via upload

---
+++ Lab4/StudentComprarer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4
+{
+    internal class StudentComparer : IComparer<Student>
+    {
+
+        public int Compare(Student? student1, Student? student2)
+        {
+
+            if (student1 == null && student2 == null)
+            {
+                return 0;
+            }
+            else if (student1! != null && student2! != null)
+            {
+                return student1.AverageGrade.CompareTo(student2.AverageGrade);
+            }
+
+            else if (student1 == null && student2 != null)
+            {
+                return -1;
+            }
+
+            else if (student1 != null && student2 == null)
+            {
+                return 1;
+            }
+
+
+
+            return 0;
+        }
+    }
+}
